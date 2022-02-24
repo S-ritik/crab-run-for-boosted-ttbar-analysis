f7db7d90ee6eb1ff97d2ad04580853391dda338c
S-ritik <57463980+S-ritik@users.noreply.github.com>
2022-02-24 13:26:46 +0530

    First Commit - All codes updated

---
+++ L5_Cor/MC/test/UL18_Runs/crabConfig_MC_JERCL5_DilepTTBar.py
@@ -0,0 +1,25 @@
+from CRABClient.UserUtilities import config #, getUsernameFromSiteDB
+config = config()
+config.General.requestName = 'crab_L5JERC_TTBar_DiLeptonic_Summer20UL18_weight'
+config.General.workArea = 'crab_L5JERC_TTBar_DiLeptonic_Summer20UL18_testRun'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'Read_Weight_cfg.py' #'JEC_MC_MINIAOD_cfg.py' 
+#config.JobType.inputFiles = ['Summer19UL18_V5_MC','Summer19UL18_JRV2_MC_SF_AK8PFPuppi.txt','Summer19UL18_JRV2_MC_SF_AK4PFchs.txt','Summer19UL18_JRV2_MC_PtResolution_AK4PFchs.txt','Summer19UL18_JRV2_MC_PtResolution_AK8PFPuppi.txt']
+config.JobType.disableAutomaticOutputCollection = True
+config.JobType.outputFiles = ['hist_jerc_l5.root','rootuple_jerc_l5.root']
+config.JobType.maxJobRuntimeMin = 2700
+#config.JobType.maxJobRuntimeMin = 1500
+config.JobType.maxMemoryMB = 2480
+config.JobType.allowUndistributedCMSSW = True
+#config.Data.inputDataset = '/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/RunIISummer19UL18MiniAOD-106X_upgrade2018_realistic_v11_L1v1-v2/MINIAODSIM'
+config.Data.inputDataset = '/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL18MiniAOD-106X_upgrade2018_realistic_v11_L1v1-v2/MINIAODSIM'
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 10
+config.Data.outLFNDirBase = '/store/user/rsaxena/' #% (getUsernameFromSiteDB())
+config.Data.publication = False
+config.Site.storageSite = 'T2_IN_TIFR'
+config.Site.blacklist=['T2_US_Caltech','T2_US_UCSD']
+config.Site.ignoreGlobalBlacklist= False
